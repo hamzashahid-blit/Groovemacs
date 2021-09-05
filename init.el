c509d8d398c951c639acbaa89e3ab85cc1ee468b
hamzashahid.blit <hamzashahid@tutanota.com>
2021-09-06 00:02:38 +0500

    Fixed fonts to be simpler AND work with Emacsclient the same.

--- init.el
+++ init.el
@@ -35,3 +35,26 @@
 (load-file my-config-file)
 
 ;;; init.el ends here
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-minibuffer-history-key "M-p")
+ '(package-selected-packages
+    '(org-bullets yasnippet-snippets yafolding which-key web-mode vterm visual-regexp-steroids use-package undo-tree try treemacs sublimity smooth-scrolling slime restart-emacs ranger rainbow-delimiters quelpa projectile pdf-tools org-superstar org-download olivetti nswbuff nix-mode nim-mode meow magit lua-mode js2-mode j-mode ivy-rich ivy-prescient ivy-posframe hindent highlight-indent-guides helpful helm-dash haskell-mode good-scroll gnu-apl-mode general fold-this flx evil-surround evil-smartparens evil-paredit evil-owl evil-org evil-nerd-commenter evil-multiedit evil-mc evil-matchit evil-lion evil-goggles evil-exchange evil-collection eterm-256color emmet-mode eimp doom-themes doom-modeline counsel company-auctex command-log-mode cmake-mode cl-libify centaur-tabs boon auto-dictionary auto-complete auctex-latexmk anzu all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
