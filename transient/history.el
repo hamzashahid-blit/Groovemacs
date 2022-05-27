a690490c4ecd4252539d0a4baa750b6fe0b68e4c
hamzashahid-blit <hamzashahid.blit.git@protonmail.com>
2022-05-27 15:03:17 +0500

    Moving from NixOS to Void, minor changes

--- transient/history.el
+++ transient/history.el
@@ -1,4 +1,7 @@
 ((magit-branch nil)
   (magit-commit nil)
   (magit-merge nil)
-  (magit-push nil))
+  (magit-push nil)
+  (magit-remote
+	("-f"))
+  (magit-remote\.<remote>\.*url "git@gitlab.com:hamzashahid.blit/peteach.git" "git@github.com:hamzashahid-blit/peteach"))
